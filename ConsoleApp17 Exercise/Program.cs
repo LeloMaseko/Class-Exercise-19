7269385a90eb0dc13db05f065c755e387404099f
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 08:59:11 +0200

    Add project files.

---
+++ ConsoleApp17 Exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The while loop for reference");
+
+            int i = 0;
+
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("This a for loop");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
